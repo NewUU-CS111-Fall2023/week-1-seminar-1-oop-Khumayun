8634d6d63583ab26f81b8943acae7d34d9017cad
Khumoyun Aminaddinov <k.aminaddinov@innopolis.university>
2023-10-13 16:53:12 +0500

    Examples for OOP

--- src/main.cpp
+++ src/main.cpp
@@ -6,30 +6,52 @@
 
 #include <iostream>
 #include "task-1.h"
-//#include "task-2.h"
-#include "task-3.h"
+
 
 int main() {
     std::cout << "Task 1" << std::endl;
-    Summation numbers;
-    std::cout << numbers.sum(1, 2) << std::endl;
+    Desk desk610;
+//    std::cout << sizeof(desk610);
+    desk610.smart = true;
+    desk610.fixed = true;
+    desk610.reflective = false;
+    desk610.width = 100.43;
+    desk610.height = 50.33;
+    desk610.changeTimeProduction(15);
+    std::cout << "Time: " << desk610.getTimeProduction() << std::endl;
+    std::cout << "Reflective: " << desk610.reflective << std::endl;
+    std::cout << "Size: " << desk610.height << "x" << desk610.width << std::endl;
+    std::cout << "Fixed: " << desk610.fixed << std::endl;
+    std::cout << "Smart: " << desk610.smart << std::endl;
+    std::cout << "Color: " << desk610.color << std::endl;
 
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    Person p("Alisher", "Navoi");
-//    p.setName();
-    std::cout << p.getFullName() << std::endl;
+    std::cout << std::endl << "Task 2" << std::endl;
+    Whiteboard wh610;
 
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    Employer emp("Mark", "Z");
-    emp.setHours(40);
-    emp.setRoom(304);
-    std::cout << emp.getFullInfo() << std::endl;
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
+    wh610.smart = false;
+    wh610.fixed = false;
+    wh610.reflective = false;
+    wh610.width = 111;
+    wh610.height = 5000;
+//    wh610.height = "fref";
+    wh610.changeTimeProduction(93);
+    wh610.magnetism = 32;
+    std::cout << "Time: " << wh610.getTimeProduction() << std::endl;
+    std::cout << "Reflective: " << wh610.reflective << std::endl;
+    std::cout << "Size: " << wh610.height << "x" << wh610.width << std::endl;
+    std::cout << "Fixed: " << wh610.fixed << std::endl;
+    std::cout << "Smart: " << wh610.smart << std::endl;
+    std::cout << "Color: " << wh610.getColor() << std::endl;
 
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
+    std::cout << std::endl << "Task 3" << std::endl;
+    Trangle tr;
+    Circle cr;
+    Rectangle rec;
+    Pentagone pen;
+
+    std::cout << "Triangle: " << tr.calculateArea() << std::endl;
+    std::cout << "Circle: " << cr.calculateArea() << std::endl;
+    std::cout << "Rectangle: " << rec.calculateArea() << std::endl;
+    std::cout << "Pentagone: " << pen.calculateArea() << std::endl;
     return 0;
 }
--- src/task-1.h
+++ src/task-1.h
@@ -4,14 +4,96 @@
  * Name:
  */
 
-class Summation {
+#include "math.h"
+
+class Desk{
 private:
-    int a, b, c;
+    int time_production;
 public:
-    int sum(int x, int y) {
-        a = x;
-        b = y;
-        c = a + b;
-        return c;
+    std::string color;
+    bool reflective;
+    float height;
+    float width;
+    bool fixed;
+    bool smart;
+
+    void changeTimeProduction(int v){
+        time_production = v*3247;
+    }
+    int getTimeProduction(){
+        std::cout << time_production;
+        return time_production;
+    }
+};
+
+class Whiteboard: public Desk{
+private:
+    std::string color = "white";
+public:
+    int magnetism;
+    std::string getColor(){
+        return color;
+    }
+};
+
+
+class Figure {
+private:
+    int amount_sides=1;
+    int amount_angles;
+    int no_vertices;
+    int diagonals;
+//    float sides[amount_sides];
+public:
+    float perimeter;
+    float area;
+    float calculateArea(){
+        std::cout << "Area is calculated";
+    };
+};
+
+class Trangle: public Figure{
+private:
+    float height=6;
+    float base=5;
+public:
+    float calculateArea(){
+        area = height*base/2;
+        return area;
+    };
+    float calculatePerimeter(){
+        perimeter = height + base; //wong
+    }
+};
+class Rectangle: public Figure{
+private:
+    float height=3, width=4;
+public:
+    float calculateArea(){
+        area = height*width;
+        return area;
+    };
+    float calculatePerimeter(){
+        perimeter = height*2+width*2;
+        return perimeter;
+    }
+};
+class Circle: public Figure{
+private:
+    float radius=13;
+public:
+    float calculateArea(){
+        area = 3.14*radius*radius;
+        return area;
+    }
+};
+class Pentagone: public Figure{
+private:
+    int number_sides=1;
+    float height=3;
+public:
+    float calculateArea(){
+        area = number_sides*height;
+        return area;
     }
 };
